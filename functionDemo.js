1f26274aa327e5c27de27fecdeedf5952b1630bd
shuvradeep1 <sddatta505@gmail.com>
2025-05-10 16:28:55 +0530

    new File Add Commit

---
+++ functionDemo.js
@@ -0,0 +1,23 @@
+
+function sayHello() {
+    console.log("S");
+      console.log("H");
+        console.log("U");
+          console.log("V");
+            console.log("R");
+              console.log("A");
+                console.log("D");
+                console.log("E");
+                console.log("E");
+                console.log("P");
+                console.log("D");
+                console.log("A");
+                console.log("T");
+                console.log("T");
+                console.log("A");
+}
+
+
+sayHello();
+
+
