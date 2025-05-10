f576928aa3ea00ab2e6f503f55c3238a948ecdd5
shuvradeep1 <sddatta505@gmail.com>
2025-05-10 19:15:02 +0530

    Some Changes.

--- functionDemo.js
+++ functionDemo.js
@@ -24,8 +24,10 @@ sayHello();
 //Sum of Two Number
 
 function addTwoNumber(a,b) {
-    if (typeof a==="number" && typeof b==="number") {
+    if (typeof a==="number" && typeof  b==="number") {
+        
          console.log("Sum of Two number is",(a+b)); 
+         return a+b;
     } else {
         console.log("a and b was not a number.");
         
@@ -33,4 +35,68 @@ function addTwoNumber(a,b) {
    
 }
 
-addTwoNumber("5",6);
+const result = addTwoNumber(5,6);
+console.log(result);
+
+
+function loginUserMessage(username){
+    if(!username ){
+        console.log("Please Enter Username");
+        return 
+        
+    }
+    console.log("User Name is ",username);
+    return `the user ${username} is logged in.`
+}
+
+const loginUser = loginUserMessage();
+console.log(loginUser);
+
+
+function calculateCartPrice(...numbers){
+    
+    return numbers
+}
+
+console.log(calculateCartPrice(23,45,67));
+
+const user ={
+    username :"Shuvradeep",
+    age: 25,
+    price: 5000
+
+}
+
+function handleObject(anyObject){
+console.log(`Username is ${anyObject.username} and age is ${anyObject.age} and price is ${anyObject.price}`);
+
+}
+
+
+handleObject(user);
+
+
+const a = 300;
+
+if(true){
+  const  a = 43;
+
+}
+console.log(a);
+
+
+var b = 5;
+
+if (true) {
+    var b= 56;
+}
+
+console.log(b);
+
+const asd = () => console.log("Getting Data...");
+
+
+
+asd();
+
+
